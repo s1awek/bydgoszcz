9492fc956679f68a85450ab00b6174afe510f58f
Slawek Kasprzak <slawomir.kasprzak@oxmedia.pl>
2018-09-18 22:42:56 +0200

    Added some icons and logos, basic layout done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-
+    $('.tabs__item').click(function(e){
+        e.preventDefault();
+        if(!$(this).hasClass('active')){
+            $('.tabs__item').removeClass('active');
+            $(this).addClass('active');
+            if($(this).hasClass('tabs__item--planned') && $(this).hasClass('active')) {
+                $('.investitions-content--planned').show();
+                $('.investitions-content--in-progress').hide();
+            }
+            if($(this).hasClass('tabs__item--in-progress') && $(this).hasClass('active')) {
+                $('.investitions-content--planned').hide();
+                $('.investitions-content--in-progress').show();
+            }
+        }
+    });
 });
