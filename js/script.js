23b85acdbf8aa6bf24c75b5e69fb696ffa9f61f9
Slawek Kasprzak <slawomir.kasprzak@oxmedia.pl>
2018-09-20 21:34:34 +0200

    Finished resposivity v1.0

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
     $('.tabs__item').click(function(e){
         e.preventDefault();
-        if(!$(this).hasClass('active')){
-            $('.tabs__item').removeClass('active');
-            $(this).addClass('active');
-            if($(this).hasClass('tabs__item--planned') && $(this).hasClass('active')) {
-                $('.investitions-content--planned').show();
-                $('.investitions-content--in-progress').hide();
+        if(window.matchMedia('(min-width: 992px)').matches || window.matchMedia('(max-width: 767px)').matches){
+            if(!$(this).hasClass('active')){
+                $('.tabs__item').removeClass('active');
+                $(this).addClass('active');
+                if($(this).hasClass('tabs__item--planned') && $(this).hasClass('active')) {
+                    $('.investitions-content--planned').show();
+                    $('.investitions-content--in-progress').hide();
+                }
+                if($(this).hasClass('tabs__item--in-progress') && $(this).hasClass('active')) {
+                    $('.investitions-content--planned').hide();
+                    $('.investitions-content--in-progress').show();
+                }
             }
-            if($(this).hasClass('tabs__item--in-progress') && $(this).hasClass('active')) {
-                $('.investitions-content--planned').hide();
-                $('.investitions-content--in-progress').show();
-            }
+        } else {
+
+        }
+    });
+
+    $(window).on('resize', function(){
+        if(window.matchMedia('(min-width: 992px)').matches || window.matchMedia('(max-width: 767px)').matches){
+            $('.investitions-content').css('display','');
         }
     });
 });
